9ea4ec4d9c56fc05a7a294797cb1204bf10850f8
Theo Selenskiy <drumandbassbob@gmail.com>
2025-03-20 14:34:19 +0000

    feat: ping

---
+++ src/app/(unauth)/ping/page.tsx
@@ -0,0 +1,14 @@
+import { ssrFetch } from "@/lib/server-side-fetching";
+
+export default async function Ping() {
+  const [response, error] = await ssrFetch<{message:string}>("/ping", {method: "GET"});
+
+  if (error || !response || "error" in response) {
+    console.error(error);
+    throw new Error("Failed to ping backend");
+  }
+
+  return(
+    <div>Backend ping: {response.message}</div>
+  )
+}
